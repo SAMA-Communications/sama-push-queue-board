2457454c3f419a64a1f5fb285d6c33172d22b8c1
Igor Khomenko <khomenkoigor+1@gmail.com>
2023-05-31 15:36:24 +0300

    cleanup

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const { createBullBoard } = require('@bull-board/api');
 const { BullMQAdapter } = require('@bull-board/api/bullMQAdapter');
 const { FastifyAdapter } = require('@bull-board/fastify');
-const { Queue: QueueMQ, Worker } = require('bullmq');
+const { Queue: QueueMQ } = require('bullmq');
 const fastify = require('fastify');
 
 require('dotenv').config()
-
-const sleep = (t) => new Promise((resolve) => setTimeout(resolve, t * 1000));
 
 const redisOptions = {
   port: process.env.REDIS_PORT,
@@ -17,28 +15,8 @@ const redisOptions = {
 
 const createQueueMQ = (name) => new QueueMQ(name, { connection: redisOptions });
 
-async function setupBullMQProcessor(queueName) {
-  new Worker(
-    queueName,
-    async (job) => {
-      for (let i = 0; i <= 100; i++) {
-        await sleep(Math.random());
-        await job.updateProgress(i);
-        await job.log(`Processing job at interval ${i}`);
-
-        if (Math.random() * 200 < 1) throw new Error(`Random error ${i}`);
-      }
-
-      return { jobId: `This is the return value of job (${job.id})` };
-    },
-    { connection: redisOptions }
-  );
-}
-
 const run = async () => {
   const exampleBullMq = createQueueMQ(process.env.BULL_QUEUE_NAME);
-
-  await setupBullMQProcessor(exampleBullMq.name);
 
   const app = fastify();
 
