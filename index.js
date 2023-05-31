3c6a89d5a0e7dfae9bac4b109c73ea52de7fbf54
Igor Khomenko <khomenkoigor+1@gmail.com>
2023-05-31 15:40:09 +0300

    renamed envs; cleanup

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 
 const redisOptions = {
   port: process.env.REDIS_PORT,
-  host: process.env.REDIS_URL,
+  host: process.env.REDIS_HOST,
   password: '',
   tls: false,
 };
@@ -30,29 +30,12 @@ const run = async () => {
   serverAdapter.setBasePath('/ui');
   app.register(serverAdapter.registerPlugin(), { prefix: '/ui' });
 
-  // app.get('/add', (req, reply) => {
-  //   const opts = req.query.opts || {};
-
-  //   if (opts.delay) {
-  //     opts.delay = +opts.delay * 1000; // delay must be a number
-  //   }
-
-  //   exampleBullMq.add('Add', { title: req.query.title }, opts);
-
-  //   reply.send({
-  //     ok: true,
-  //   });
-  // });
-
   await app.listen({ port: process.env.PORT });
+  
   // eslint-disable-next-line no-console
   console.log(`Running on ${process.env.PORT}...`);
   console.log(`For the UI, open http://localhost:${process.env.PORT}/ui`);
   console.log(`Make sure Redis is running on port ${process.env.REDIS_PORT} by default`);
-  // console.log('To populate the queue, run:');
-  // console.log(`  curl http://localhost:${process.env.PORT}/add?title=Example`);
-  // console.log('To populate the queue with custom options (opts), run:');
-  // console.log(`  curl http://localhost:${process.env.PORT}/add?title=Test&opts[delay]=9`);
 };
 
 run().catch((e) => {
