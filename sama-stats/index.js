4ad3125d220a53bcb15075262b62f689e0eb42d5
AntonSeleznev <banshi.anton@gmail.com>
2026-06-19 12:33:16 +0300

    it can check multiple nodes (#5)
    * it can check multiple nodes

--- sama-stats/index.js
+++ sama-stats/index.js
@@ -1,15 +1,19 @@
 const path = require('node:path')
+const Redis = require('ioredis')
 const pointOfView = require('@fastify/view')
 
+let redisConnection = void 0
+let lastFetchDate = void 0
 let lastServerStats = {}
 
-const fetchSamaServerStats = async () => {
+const fetchSamaServerStats = async (endpoint) => {
+  endpoint = endpoint.replace(/\/$/, '')
+
   const serverStats = await fetch(
-      `${process.env.SAMA_URL}/admin/server-stats?format=1`,
+      `${endpoint}/admin/server-stats?format=1`,
       { method: 'GET', headers: {'Admin-Api-Key': process.env.SAMA_ADMIN_API_KEY} }  
     )
     .then(response => response.json())
-    .then(serverStats => ({ serverStats: Object.assign(serverStats, { fetchDate: new Date().toString() }) }))
     .catch(error => {
       console.log('[Error][fetching]', error)
       const message = `SAMA Server: ${error.message}`
@@ -19,13 +23,49 @@ const fetchSamaServerStats = async () => {
   return serverStats
 }
 
-const startFetchingServerDate = () => {
-  setInterval(async () => {
-    lastServerStats = await fetchSamaServerStats()
-  }, process.env.SERVER_UPDATE_INTERVAL ?? 30_000)
+const fetchClusterStats = async () => {
+  let clusterEndpoints = []
+
+  if (process.env.SAMA_URL) {
+    clusterEndpoints = process.env.SAMA_URL.split(',')
+  } else {
+    const listNodes = await redisConnection.keys('sama-node-data:*')
+
+    clusterEndpoints = listNodes
+      .map(key => key.replace('sama-node-data:', ''))
+      .map(wsEndpoint => {
+        const url = new URL(wsEndpoint)
+        url.protocol = 'http'
+        url.port = 9001
+        return url.toString()
+      })
+  }
+
+  console.log('[Endpoints]', clusterEndpoints)
+
+  const clusterStats = {}
+  
+  for (const clusterEndpoint of clusterEndpoints) {
+    const stats = await fetchSamaServerStats(clusterEndpoint)
+
+    console.log('[Stats]', clusterEndpoint, stats)
+
+    clusterStats[clusterEndpoint] = stats
+  }
+
+  return clusterStats
 }
 
-module.exports = (fastifyApp) => {
+const updateClusterStats = async () => {
+  lastServerStats = await fetchClusterStats()
+  lastFetchDate = new Date().toString()
+}
+
+const startFetchingServerStats = () => setInterval(updateClusterStats, process.env.SERVER_UPDATE_INTERVAL ?? 30_000)
+
+module.exports = (fastifyApp, redisOptions) => {
+  redisConnection = new Redis(redisOptions)
+
   fastifyApp.register(pointOfView, {
     engine: {
       ejs: require('ejs'),
@@ -37,7 +77,9 @@ module.exports = (fastifyApp) => {
     method: 'GET',
     url: '/stats/sama-server',
     handler: (req, reply) => {
-      reply.view('sama-server-stats.ejs', { updateTime: process.env.CLIENT_UPDATE_INTERVAL ?? 10_000 });
+      reply.view('sama-server-stats.ejs', {
+        updateTime: process.env.CLIENT_UPDATE_INTERVAL ?? 10_000,
+      });
     },
   });
   
@@ -45,9 +87,9 @@ module.exports = (fastifyApp) => {
     method: 'GET',
     url: '/stats/data/sama-server',
     handler: async (req, reply) => {  
-      reply.send(lastServerStats)
+      reply.send({ fetchDate: lastFetchDate, stats: lastServerStats })
     },
   });
 
-  startFetchingServerDate()
+  startFetchingServerStats()
 }
